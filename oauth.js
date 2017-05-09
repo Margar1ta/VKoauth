6e755ed2ea7b63ed0efd3940438ba658bf0af0cc
Iakovleva Margarita <naruyaoi@yandex.ru>
2017-05-09 18:45:17 +0300

    Update oauth.js

--- oauth.js
+++ oauth.js
@@ -1,3 +1,25 @@
 function oauth(){
   alert( "Привет" );
+  var params=getParams();
+
+var code=params['code'];
+  
+}
+
+
+function getParams(){
+  var params = window
+    .location
+    .search
+    .replace('?','')
+    .split('&')
+    .reduce(
+        function(arr,param){
+            var a = param.split('=');
+            arr[ decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
+            return arr;
+        },
+        {}
+    ); 
+  return params;
 }
